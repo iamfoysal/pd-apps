bf53e356fbeb221c972e835db5b91acd0a37526c
foysal <foysalf652@gmail.com>
2022-01-29 22:34:45 +0600

    useing  widget and add forms attrs added

--- app1/forms.py
+++ app1/forms.py
@@ -1,8 +1,14 @@
+from datetime import date
 from django import forms
+
 
 
 class user_form(forms.Form):
        #<label for="user_name">Full Name</label>
         #<input type="text" name="user_name" value="" required>
-    user_name =forms.CharField(required=True, initial="Mohammad Foysal", label="Full Name")
-    user_email = forms.EmailField(label="User Email") 
+    user_name =forms.CharField( label="Full Name", widget=forms.TextInput(attrs={'placeholder':'Enter your full name', 'style':'width:300px'}))
+    
+    user_dob =forms.DateField(label="Date Of Birth", widget=forms.TextInput(attrs={'type':'date'}))
+    
+    user_email = forms.EmailField(label="User Email", widget=forms.TextInput(attrs={'placeholder':'Enter Your Email'})) 
+    
