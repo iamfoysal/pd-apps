168201496401bcd5cc8383a252b39b0ee05fa258
foysal <foysalf652@gmail.com>
2022-01-29 21:16:50 +0600

    Form fields,required,label,initial

--- app1/forms.py
+++ app1/forms.py
@@ -1,9 +1,8 @@
-from tkinter.tix import Form
 from django import forms
 
 
 class user_form(forms.Form):
        #<label for="user_name">Full Name</label>
         #<input type="text" name="user_name" value="" required>
-    user_name =forms.CharField()
-    user_email = forms.EmailField() 
+    user_name =forms.CharField(required=True, initial="Mohammad Foysal", label="Full Name")
+    user_email = forms.EmailField(label="User Email") 
