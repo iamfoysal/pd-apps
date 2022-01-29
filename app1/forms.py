9c413ab06bda2e3ec7c37d49af4ac83c790ef95c
foysal <foysalf652@gmail.com>
2022-01-30 00:24:56 +0600

    form submission completed but phone or Email Field note working

--- app1/forms.py
+++ app1/forms.py
@@ -4,11 +4,13 @@ from django import forms
 
 
 class user_form(forms.Form):
-       #<label for="user_name">Full Name</label>
-        #<input type="text" name="user_name" value="" required>
-    user_name =forms.CharField( label="Full Name", widget=forms.TextInput(attrs={'placeholder':'Enter your full name', 'style':'width:300px'}))
+       # when use html tag like this <label for="user_name">Full Name</label>
+       #when use html tag like this <input type="text" name="user_name" value="" required>
+       
+    user_name =forms.CharField( label="Full Name", widget=forms.TextInput(attrs={'placeholder':'Enter your full name', 'style':'padding:6px'}))
     
-    user_dob =forms.DateField(label="Date Of Birth", widget=forms.TextInput(attrs={'type':'date'}))
+    user_dob =forms.DateField(label="Date Of Birth", widget=forms.TextInput(attrs={'type':'date','style':'padding:6px'}))
+    # user_phone =forms.IntegerField(label="User Phone", widget=forms.NumberInput(attrs={'placeholder':'+880**********','style':'padding:6px','type':'number'}))
     
-    user_email = forms.EmailField(label="User Email", widget=forms.TextInput(attrs={'placeholder':'Enter Your Email'})) 
+    # user_email = forms.EmailField(label="User Email", widget=forms.TextInput(attrs={'placeholder':'Enter Your Email','style':'padding:6px'})) 
     
--- app1/views.py
+++ app1/views.py
@@ -16,5 +16,19 @@ def about(request):
 def form (request):
     new_form = forms.user_form()
     diction  = {'test_form':new_form, 'heading_1':"This form is created using Django libary"}
+    if request.method == 'POST':
+        new_form =forms.user_form(request.POST)
+        if new_form.is_valid():
+            user_name = new_form.cleaned_data['user_name']
+            user_dob =new_form.cleaned_data['user_dob']
+            # user_phone = new_form.changed_data['user_phone']
+            user_email = new_form.changed_data['user_email']  
+            
+            diction.update({'user_name':user_name})       
+            diction.update({'user_dob':user_dob})
+            # diction.update({'user_phone':user_phone})
+            # diction.update({'user_email':user_email})
+            diction.update({'form_submited':"Yes"})
+            
     return render (request, 'app1/form.html', context=diction) 
     
