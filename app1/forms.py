afb0ca949557e261444128675e79fafb97a7a167
foysal <foysalf652@gmail.com>
2022-02-05 13:20:58 +0600

    some fields

--- app1/forms.py
+++ app1/forms.py
@@ -1,16 +1,19 @@
 from datetime import date
+from xmlrpc.client import boolean
 from django import forms
 
 
 
 class user_form(forms.Form):
        # when use html tag like this <label for="user_name">Full Name</label>
-       #when use html tag like this <input type="text" name="user_name" value="" required>
+       # when use html tag like this <input type="text" name="user_name" value="" required>
        
-    user_name =forms.CharField( label="Full Name", widget=forms.TextInput(attrs={'placeholder':'Enter your full name', 'style':'padding:6px'}))
+    user_name =forms.CharField( label="Full Name", required=False, widget=forms.TextInput(attrs={'placeholder':'Enter your full name', 'style':'padding:6px'}))
     
-    user_dob =forms.DateField(label="Date Of Birth", widget=forms.TextInput(attrs={'type':'date','style':'padding:6px'}))
-    # user_phone =forms.IntegerField(label="User Phone", widget=forms.NumberInput(attrs={'placeholder':'+880**********','style':'padding:6px','type':'number'}))
+    user_dob =forms.DateField(label="Date Of Birth",required=False, widget=forms.TextInput(attrs={'type':'date','style':'padding:6px'}))
+    user_phone =forms.IntegerField(label="User Phone", required=False,widget=forms.NumberInput(attrs={'placeholder':'+880**********','style':'padding:6px','type':'number'}))
+    user_email = forms.EmailField(label="User Email", required=False,  widget=forms.TextInput(attrs={'placeholder':'Enter Your Email','style':'padding:6px'})) 
+    address_field = forms.CharField(max_length=40, min_length=10)
+    boolean_field = forms.BooleanField(required=False) 
     
-    # user_email = forms.EmailField(label="User Email", widget=forms.TextInput(attrs={'placeholder':'Enter Your Email','style':'padding:6px'})) 
-    
+   #  set_password = forms.PasswordInput() 
--- app1/views.py
+++ app1/views.py
@@ -15,19 +15,26 @@ def about(request):
 
 def form (request):
     new_form = forms.user_form()
-    diction  = {'test_form':new_form, 'heading_1':"This form is created using Django libary"}
+    diction  = {'test_form':new_form, 'heading_1':"This is user Form created in Django library"}
     if request.method == 'POST':
         new_form =forms.user_form(request.POST)
         if new_form.is_valid():
             user_name = new_form.cleaned_data['user_name']
             user_dob =new_form.cleaned_data['user_dob']
-            # user_phone = new_form.changed_data['user_phone']
-            user_email = new_form.changed_data['user_email']  
+            user_phone = new_form.cleaned_data['user_phone']
+            user_email = new_form.cleaned_data['user_email']  
+            boolean_field = new_form.cleaned_data['boolean_field']
+            address_field= new_form.cleaned_data['address_field']
+            # set_password = new_form.cleaned_data['set_password']
             
+            
+            # diction.update({'set_password': set_password})
+            diction.update({'address_field':address_field})
+            diction.update({'boolean_field':boolean_field})
             diction.update({'user_name':user_name})       
             diction.update({'user_dob':user_dob})
-            # diction.update({'user_phone':user_phone})
-            # diction.update({'user_email':user_email})
+            diction.update({'user_phone':user_phone})
+            diction.update({'user_email':user_email})
             diction.update({'form_submited':"Yes"})
             
     return render (request, 'app1/form.html', context=diction) 
